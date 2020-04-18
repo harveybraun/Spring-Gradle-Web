003b9161eaab74b6f5d4bab93b9ec303b5f10049
Harvey Braun <63766520+harveybraun@users.noreply.github.com>
2020-04-18 18:17:41 -0300

    Add files via upload

---
+++ Spring - Gradle - Web/src/main/java/braun/harvey/exemplo/ExemploApplication.java
@@ -0,0 +1,20 @@
+package braun.harvey.exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploApplication.class, args);
+
+		System.out.println("Exemplo com Servidor Tomcat");
+		
+		// Teste - localhost:8080
+		
+		// Teste - localhost:8091
+		
+	}
+
+}
